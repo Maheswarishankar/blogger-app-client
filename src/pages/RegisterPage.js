9542f5a386c01270f152077ad067fafc89cc7f25
Maheswari.S <mahi.nawins@gmail.com>
2023-09-08 13:25:34 +0530

    first commit

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,35 @@
+import {useState} from "react";
+
+export default function RegisterPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  async function register(ev) {
+    ev.preventDefault();
+    const response = await fetch('https://blogger-app-5qxr.onrender.com/register', {
+      method: 'POST',
+      body: JSON.stringify({username,password}),
+      headers: {'Content-Type':'application/json'},
+      credentials: 'include',
+    });
+    if (response.status === 200) {
+      alert('registration successful');
+    } else {
+      alert('registration failed');
+    }
+  }
+  return (
+    
+    <form className="register" onSubmit={register}>
+      <h1>SignUp</h1>
+      <input type="text"
+             placeholder="username"
+             value={username}
+             onChange={ev => setUsername(ev.target.value)}/>
+      <input type="password"
+             placeholder="password"
+             value={password}
+             onChange={ev => setPassword(ev.target.value)}/>
+      <button>Register</button>
+    </form>
+  );
+}
